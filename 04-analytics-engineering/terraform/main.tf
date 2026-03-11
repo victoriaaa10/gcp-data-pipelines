eceb90fc3bcef2677af00a6a06946f99cd0198d3
Victoria Torreno <163030280+vtorreno@users.noreply.github.com>
2026-03-10 23:38:37 -0700

    update module 4

--- 04-analytics-engineering/terraform/main.tf
+++ 04-analytics-engineering/terraform/main.tf
@@ -8,22 +8,26 @@ terraform {
 }
 
 provider "google" {
+  credentials = file("../google_credentials.json")
   project = var.project
   region  = var.region
 }
 
+# Bronze Dataset
 resource "google_bigquery_dataset" "raw_dataset" {
   dataset_id                 = var.bq_dataset_raw
   location                   = var.location
   delete_contents_on_destroy = true 
 }
 
+# Silver/Gold Dataset
 resource "google_bigquery_dataset" "dbt_dataset" {
   dataset_id                 = var.bq_dataset_dbt
   location                   = var.location
   delete_contents_on_destroy = true
 }
 
+# Data Lake Bucket
 resource "google_storage_bucket" "data_lake" {
   name                        = var.gcs_bucket_name
   location                    = var.location
